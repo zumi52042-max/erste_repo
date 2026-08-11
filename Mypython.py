ffd090eb28369d1ddad0e8d50eb7a6ef5ed1cc27
zumi <zumi52042@gmail.com>
2026-08-11 22:15:03 +0200

    Statistische Lerndaten V2

--- Mypython.py
+++ Mypython.py
@@ -42,31 +42,31 @@ if __name__ == "__main__":
  #   """
   #  Calculate the number of courses and users in the JSON file.
    # JSON file is located at ~/project/user_study.json
-#
-    Returns:
+
+#    Returns:
  #       count_user(int): Total number of users.
   #      count_course(int): Total number of courses.
    # """
-#
+
  #   count_user = 0
   #  count_course = 0
-#
+
  #   with open("user_study.json", "r") as f:
   #      data = json.load(f)
-#
+
  #   count_user_set = set()  # Create an empty set to store unique user IDs
     count_course_set = set()  # Create an empty set to store unique course names
-#
- ##   for i in data:
+
+ #   for i in data:
    #     count_user_set.add(i["user_id"])  # Add each user ID to the set
     #    count_course_set.add(i["course"])  # Add each course name to the set
 #
  #   count_user = len(count_user_set)
     count_course = len(count_course_set)
-##
+
   #  return count_user, count_course
 
-#
+
 #if __name__ == "__main__":
     # Print the number of unique user IDs and course names
  #   count_user, count_course = count()
